169127c1886373449d7f634b66246ebfb4cca63b
shinjh0849 <shinjh0849@gmail.com>
2019-10-17 17:36:12 +0900

    added euclidean distance

--- src/main/java/change/vector/collector/Correlation.java
+++ src/main/java/change/vector/collector/Correlation.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.math3.ml.distance.EuclideanDistance;
 import org.apache.commons.math3.stat.correlation.Covariance;
 import org.apache.commons.math3.stat.correlation.KendallsCorrelation;
 import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
@@ -22,7 +23,9 @@ public class Correlation {
 		computePCC(input);
 		computeSCC(input);
 		computeKCC(input);
+		computeED(input);
 		computeCovariance(input);
+		System.out.println("writing all corelations done!");
 	}
 
 	public static void computePCC(Input input) throws Exception {
@@ -166,6 +169,54 @@ public class Correlation {
 			csvprinter.println();
 		}
 		System.out.println("writing kcc done!");
+		csvprinter.close();
+	}
+	
+	public static void computeED(Input input) throws Exception {
+		String filePath = input.inFile;
+		
+		DataSource source = null;
+		Instances dataset = null;
+		source = new DataSource(filePath);
+		dataset = source.getDataSet();
+		
+		if(dataset.classIndex() == -1)
+			dataset.setClassIndex(dataset.numAttributes() - 1);
+		
+		System.out.println("num of att: " + dataset.numAttributes());
+		System.out.println("num of inst " + dataset.numInstances());
+		// System.out.println(dataset);
+		
+		// double[][] instantiation
+		ArrayList<ArrayList<Double>> pcc = new ArrayList<ArrayList<Double>>();
+		for(int i = 0; i < dataset.numInstances(); i++) {
+			pcc.add(new ArrayList<Double>());
+			for(int j = 0; j < dataset.numInstances(); j++) {
+				pcc.get(i).add(0.0);
+			}
+		}
+		
+		// computing Covariance one by one
+		for(int i = 0; i < dataset.numInstances(); i++) {
+			double[] x = dataset.get(i).toDoubleArray();
+			for(int j = 0; j < dataset.numInstances(); j++) {
+				double[] y = dataset.get(j).toDoubleArray();
+			    pcc.get(i).set(j, new EuclideanDistance().compute(x,y));
+			}
+		}
+		
+		// writing files
+		File outFile = new File(input.outFile+"Euclidean_distance.csv");
+		BufferedWriter writer = Files.newBufferedWriter(Paths.get(outFile.getAbsolutePath()));
+		CSVPrinter csvprinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
+		for(int i = 0; i < dataset.numInstances(); i++) {
+			for(int j = 0; j < dataset.numInstances(); j++) {
+				csvprinter.print(pcc.get(i).get(j));
+				
+			}
+			csvprinter.println();
+		}
+		System.out.println("writing Euclidean Distance done!");
 		csvprinter.close();
 	}
 	
