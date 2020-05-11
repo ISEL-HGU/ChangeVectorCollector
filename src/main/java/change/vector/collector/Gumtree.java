3de4fb83812017fc1eaae9a95f1a10cdfab4e95e
shinjh0849 <shinjh0849@gmail.com>
2020-05-11 10:52:15 +0900

    adding context in GV vector

--- src/main/java/change/vector/collector/Gumtree.java
+++ src/main/java/change/vector/collector/Gumtree.java
@@ -35,9 +35,9 @@ public class Gumtree {
 		ArrayList<ArrayList<Integer>> gumtree_vectors = new ArrayList<ArrayList<Integer>>();
 		ArrayList<BeforeBIC> new_bbics = new ArrayList<BeforeBIC>();
 		final String[] header_Y = { "index", "path_before", "path_BIC", "sha_before", "sha_BIC", "path_fix", "sha_fix",
-		"key" };
+				"key" };
 		File file_Y = new File(input.outFile + "Y_" + input.projectName + ".csv");
-		File file_GV = new File(input.outFile + "GV_" + input.projectName + ".csv");
+		File file_GV = new File(input.outFile + "GVNC_" + input.projectName + ".csv");
 		BufferedWriter writer_Y = Files.newBufferedWriter(Paths.get(file_Y.getAbsolutePath()));
 		BufferedWriter writer_GV = Files.newBufferedWriter(Paths.get(file_GV.getAbsolutePath()));
 		CSVPrinter csvprinter_Y = new CSVPrinter(writer_Y, CSVFormat.DEFAULT.withHeader(header_Y));
@@ -46,27 +46,24 @@ public class Gumtree {
 		try {
 			int cnt = 0;
 			for (BeforeBIC bbic : bbics) {
-
 				RevCommit commitBIC = walk.parseCommit(repo.resolve(bbic.shaBIC));
 				RevCommit commitBBIC = walk.parseCommit(repo.resolve(bbic.shaBefore));
-				RevCommit commitFIX = walk.parseCommit(repo.resolve(bbic.shaFix));
-				RevCommit commitBFIX = walk.parseCommit(repo.resolve(bbic.shaFix + "^"));
-				
+//				RevCommit commitFIX = walk.parseCommit(repo.resolve(bbic.shaFix));
+//				RevCommit commitBFIX = walk.parseCommit(repo.resolve(bbic.shaFix + "^"));
 
 				String pathBIC = bbic.pathBIC;
 				String pathBBIC = bbic.pathBefore;
-				String pathFIX = bbic.pathFix;
-				String pathBFIX = bbic.pathBFix;
-				
+//				String pathFIX = bbic.pathFix;
+//				String pathBFIX = bbic.pathBFix;
 
 				String srcBlobBIC = Utils.fetchBlob(repo, commitBBIC.getName(), pathBBIC);
 				String dstBlobBIC = Utils.fetchBlob(repo, commitBIC.getName(), pathBIC);
-				String srcBlobFIX = Utils.fetchBlob(repo, commitBFIX.getName(), pathBFIX);
-				String dstBlobFIX = Utils.fetchBlob(repo, commitFIX.getName(), pathFIX);
-				
+//				String srcBlobFIX = Utils.fetchBlob(repo, commitBFIX.getName(), pathBFIX);
+//				String dstBlobFIX = Utils.fetchBlob(repo, commitFIX.getName(), pathFIX);
+
 				Run.initGenerators();
 
-				// for BIC 
+				// for BIC
 				ITree srcBIC;
 				ITree dstBIC;
 				try {
@@ -90,7 +87,6 @@ public class Gumtree {
 
 				ArrayList<Integer> g_vec = new ArrayList<Integer>();
 				for (Action action : actionsBIC) {
-
 					if (action.getName().equals("INS")) {
 						g_vec.add(action.getNode().getType() + 1);
 					} else if (action.getName().equals("DEL")) {
@@ -101,72 +97,83 @@ public class Gumtree {
 						g_vec.add(action.getNode().getType() + 85 * 3 + 1);
 					}
 				}
-				
+
 				// zero padding if less than MAX_SIZE
-				if(g_vec.size() < MAX_SIZE) {
-					for (int i = 0; i < MAX_SIZE - g_vec.size(); i++) {
+				if (actionsBIC.size() < MAX_SIZE) {
+					for (int i = 0; i < MAX_SIZE - actionsBIC.size(); i++) {
 						g_vec.add(0);
 					}
 				}
-				
-				
-				// for fix change
-				ITree srcFIX;
-				ITree dstFIX;
-				try {
-					srcFIX = new JdtTreeGenerator().generateFromString(srcBlobFIX).getRoot();
-					dstFIX = new JdtTreeGenerator().generateFromString(dstBlobFIX).getRoot();
-				} catch (Exception e) {
-					continue;
-				}
-				Matcher matchFIX = Matchers.getInstance().getMatcher(srcFIX, dstFIX);
-				matchFIX.match();
 
-				ActionGenerator gFIX = new ActionGenerator(srcFIX, dstFIX, matchFIX.getMappings());
-				gFIX.generate();
-
-				List<Action> actionsFIX = gFIX.getActions();
-
-				if (actionsFIX.size() > 500)
-					continue;
-				if (actionsFIX.size() <= 0)
-					continue;
-
+				// retrieving the context vector in AST
+				ArrayList<Integer> cnt_vec = new ArrayList<Integer>();
 				for (Action action : actionsBIC) {
-
-					if (action.getName().equals("INS")) {
-						g_vec.add(action.getNode().getType() + 1);
-					} else if (action.getName().equals("DEL")) {
-						g_vec.add(action.getNode().getType() + 85 + 1);
-					} else if (action.getName().equals("UPD")) {
-						g_vec.add(action.getNode().getType() + 85 * 2 + 1);
-					} else if (action.getName().equals("MOV")) {
-						g_vec.add(action.getNode().getType() + 85 * 3 + 1);
-					}
-
-				}
-				
-				// zero padding if less than MAX_SIZE
-				if(g_vec.size() < MAX_SIZE * 2) {
-					for (int i = 0; i < (MAX_SIZE * 2) - g_vec.size(); i++) {
-						g_vec.add(0);
+					List<ITree> children = action.getNode().getParent().getChildren();
+					for (ITree child : children) {
+						cnt_vec.add(child.getType());
 					}
 				}
-				
+
+//				// adding fix change
+//				ITree srcFIX;
+//				ITree dstFIX;
+//				try {
+//					srcFIX = new JdtTreeGenerator().generateFromString(srcBlobFIX).getRoot();
+//					dstFIX = new JdtTreeGenerator().generateFromString(dstBlobFIX).getRoot();
+//				} catch (Exception e) {
+//					continue;
+//				}
+//				Matcher matchFIX = Matchers.getInstance().getMatcher(srcFIX, dstFIX);
+//				matchFIX.match();
+//
+//				ActionGenerator gFIX = new ActionGenerator(srcFIX, dstFIX, matchFIX.getMappings());
+//				gFIX.generate();
+//
+//				List<Action> actionsFIX = gFIX.getActions();
+//
+//				if (actionsFIX.size() > MAX_SIZE)
+//					continue;
+//				if (actionsFIX.size() <= 0)
+//					continue;
+//
+//				for (Action action : actionsBIC) {
+//					if (action.getName().equals("INS")) {
+//						g_vec.add(action.getNode().getType() + 1);
+//					} else if (action.getName().equals("DEL")) {
+//						g_vec.add(action.getNode().getType() + 85 + 1);
+//					} else if (action.getName().equals("UPD")) {
+//						g_vec.add(action.getNode().getType() + 85 * 2 + 1);
+//					} else if (action.getName().equals("MOV")) {
+//						g_vec.add(action.getNode().getType() + 85 * 3 + 1);
+//					}
+//				}
+//
+//				// zero padding if less than MAX_SIZE
+//				if (g_vec.size() < MAX_SIZE * 2) {
+//					for (int i = 0; i < (MAX_SIZE * 2) - g_vec.size() + MAX_SIZE; i++) {
+//						g_vec.add(0);
+//					}
+//				}
+
+				// adding the two lists
+
+				g_vec.addAll(cnt_vec);
+				System.out.println(cnt + ": " + g_vec.size());
+
 				gumtree_vectors.add(g_vec);
 				new_bbics.add(bbic);
-				
+
 				csvprinter_Y.printRecord(input.projectName + cnt, bbic.pathBefore, bbic.pathBIC, bbic.shaBefore,
 						bbic.shaBIC, bbic.pathFix, bbic.shaFix, bbic.key);
 				csvprinter_Y.flush();
-				
+
 				csvprinter_GV.print(input.projectName + cnt);
 				for (Integer val : g_vec) {
 					csvprinter_GV.print(val);
 				}
 				csvprinter_GV.println();
 				csvprinter_GV.flush();
-				
+
 				System.out.println(cnt + "/" + bbics.size());
 				cnt++;
 			}
