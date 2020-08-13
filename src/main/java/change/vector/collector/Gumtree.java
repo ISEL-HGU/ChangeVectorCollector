e33912963049200fdd097dcfe20f17fe727bd475
shinjh0849 <shinjh0849@gmail.com>
2020-08-13 14:56:57 +0900

    move funcions in gumtree class and make cnt_vec different

--- src/main/java/change/vector/collector/Gumtree.java
+++ src/main/java/change/vector/collector/Gumtree.java
@@ -44,6 +44,217 @@ import com.github.gumtreediff.tree.ITree;
 
 public class Gumtree {
 
+	public static void runGumtree(Input input, ArrayList<BeforeBIC> bbics)
+			throws MissingObjectException, IncorrectObjectTypeException, IOException {
+
+		Repository repo = input.repo;
+		RevWalk walk = new RevWalk(repo);
+		ArrayList<ArrayList<Integer>> gumtree_vectors = new ArrayList<ArrayList<Integer>>();
+		ArrayList<BeforeBIC> new_bbics = new ArrayList<BeforeBIC>();
+		File file_Y;
+		File file_GV;
+		if (Main.is_clean) {
+			file_Y = new File(input.outputDir + "Y_" + input.projectName + ".csv");
+			file_GV = new File(input.outputDir + "GVNC_" + input.projectName + ".csv");
+		} else {
+			file_Y = new File(input.outputDir + "Y_" + input.projectName + ".csv");
+			file_GV = new File(input.outputDir + "GVNC_" + input.projectName + ".csv");
+		}
+
+		BufferedWriter writer_Y = Files.newBufferedWriter(Paths.get(file_Y.getAbsolutePath()));
+		BufferedWriter writer_GV = Files.newBufferedWriter(Paths.get(file_GV.getAbsolutePath()));
+		CSVPrinter csvprinter_Y = new CSVPrinter(writer_Y, CSVFormat.DEFAULT);
+		CSVPrinter csvprinter_GV = new CSVPrinter(writer_GV, CSVFormat.DEFAULT);
+
+		int cnt = 0;
+		for (BeforeBIC bbic : bbics) {
+			RevCommit commitBIC = walk.parseCommit(repo.resolve(bbic.shaBIC));
+			RevCommit commitBBIC = walk.parseCommit(repo.resolve(bbic.shaBeforeBIC));
+			// RevCommit commitFIX = walk.parseCommit(repo.resolve(bbic.shaFix));
+			// RevCommit commitBFIX = walk.parseCommit(repo.resolve(bbic.shaFix + "^"));
+
+			String pathBIC = bbic.pathBIC;
+			String pathBBIC = bbic.pathBeforeBIC;
+			// String pathFIX = bbic.pathFix;
+			// String pathBFIX = bbic.pathBFix;
+
+			String srcBlobBIC = Utils.fetchBlob(repo, commitBBIC.getName(), pathBBIC);
+			String dstBlobBIC = Utils.fetchBlob(repo, commitBIC.getName(), pathBIC);
+			// String srcBlobFIX = Utils.fetchBlob(repo, commitBFIX.getName(), pathBFIX);
+			// String dstBlobFIX = Utils.fetchBlob(repo, commitFIX.getName(), pathFIX);
+
+			Run.initGenerators();
+
+			// for BIC
+			ITree srcBIC;
+			ITree dstBIC;
+			try {
+				srcBIC = new JdtTreeGenerator().generateFromString(srcBlobBIC).getRoot();
+				dstBIC = new JdtTreeGenerator().generateFromString(dstBlobBIC).getRoot();
+			} catch (Exception e) {
+				continue;
+			}
+			Matcher matchBIC = Matchers.getInstance().getMatcher(srcBIC, dstBIC);
+			matchBIC.match();
+
+			ActionGenerator gBIC = new ActionGenerator(srcBIC, dstBIC, matchBIC.getMappings());
+			gBIC.generate();
+
+			List<Action> actionsBIC = gBIC.getActions();
+
+			ArrayList<Integer> g_vec = new ArrayList<Integer>();
+			for (Action action : actionsBIC) {
+				// if regards import, discard
+				if (action.getNode().getType() == 40 || action.getNode().getType() == 26) {
+					continue;
+				}
+
+				if (action.getName().equals("INS")) {
+					g_vec.add(action.getNode().getType() + 1);
+				} else if (action.getName().equals("DEL")) {
+					g_vec.add(action.getNode().getType() + 85 + 1);
+				}
+			}
+
+			// using JDT parser to get the line number of AST nodes
+			@SuppressWarnings("deprecation")
+			ASTParser parser = ASTParser.newParser(AST.JLS9);
+			parser.setKind(ASTParser.K_COMPILATION_UNIT);
+			Hashtable<String, String> pOptions = JavaCore.getOptions();
+			pOptions.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_9);
+			pOptions.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_9);
+			pOptions.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_9);
+			pOptions.put(JavaCore.COMPILER_DOC_COMMENT_SUPPORT, JavaCore.ENABLED);
+			parser.setCompilerOptions(pOptions);
+			parser.setSource(dstBlobBIC.toCharArray());
+			CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+
+			// retrieving the context vector in AST
+			ArrayList<Integer> context_vec = new ArrayList<Integer>();
+			HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+			for (Action action : actionsBIC) {
+				int lineNumOfBIC = cu.getLineNumber(action.getNode().getPos());
+				int parent_hash = action.getNode().getParent().getHash();
+				if (map.containsKey(parent_hash)) {
+					continue;
+				} else {
+					map.put(parent_hash, true);
+					List<ITree> descendants = action.getNode().getParent().getDescendants();
+					for (ITree descendant : descendants) {
+						if (map.containsKey(descendant.getHash())) {
+							continue;
+						} else {
+							int lineNumOfDescendant = cu.getLineNumber(descendant.getPos());
+							if (Math.abs(lineNumOfBIC - lineNumOfDescendant) < 3) {
+								map.put(descendant.getHash(), true);
+								context_vec.add(descendant.getType() + 85 * 2 + 1);
+							}
+						}
+					}
+				}
+			}
+
+			// adding the two lists
+			g_vec.addAll(context_vec);
+			System.out.println(cnt + ": " + g_vec.size());
+
+			gumtree_vectors.add(g_vec);
+			new_bbics.add(bbic);
+
+			csvprinter_Y.printRecord(input.projectName + cnt, bbic.pathBeforeBIC, bbic.pathBIC, bbic.shaBeforeBIC,
+					bbic.shaBIC, bbic.pathBeforeBFC, bbic.pathBFC, bbic.shaBeforeBFC, bbic.shaBFC, bbic.key,
+					input.projectName, bbic.label);
+
+			csvprinter_Y.flush();
+
+			for (Integer val : g_vec) {
+				csvprinter_GV.print(val);
+			}
+			csvprinter_GV.println();
+			csvprinter_GV.flush();
+
+			System.out.println(cnt + "/" + bbics.size());
+			cnt++;
+		}
+
+		System.out.println("writing gumvecs complete!");
+
+		csvprinter_Y.close();
+		csvprinter_GV.close();
+		walk.close();
+	}
+
+	public static void writeGumVecs(Input input, ArrayList<ArrayList<Integer>> gumtree_vectors) throws IOException {
+		File fileP = new File(input.outputDir + "GV_" + input.projectName + ".csv");
+		BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileP.getAbsolutePath()));
+		CSVPrinter csvprinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
+
+		int index = 0;
+		for (ArrayList<Integer> g_vec : gumtree_vectors) {
+			csvprinter.print(input.projectName + index);
+			for (Integer val : g_vec) {
+				csvprinter.print(val);
+			}
+			csvprinter.println();
+			csvprinter.flush();
+			index++;
+		}
+
+		csvprinter.close();
+		System.out.println("wrting gumvecs complete!");
+		return;
+	}
+
+	public static int getMaxASTLenth(ArrayList<BeforeBIC> bbics, Input input) throws RevisionSyntaxException,
+			MissingObjectException, IncorrectObjectTypeException, AmbiguousObjectException, IOException {
+		Repository repo = input.repo;
+		RevWalk walk = new RevWalk(repo);
+		int max_size = 0;
+		// get the max length of changes
+		for (BeforeBIC bbic : bbics) {
+			RevCommit commitBIC = walk.parseCommit(repo.resolve(bbic.shaBIC));
+			RevCommit commitBBIC = walk.parseCommit(repo.resolve(bbic.shaBeforeBIC));
+
+			String pathBIC = bbic.pathBIC;
+			String pathBBIC = bbic.pathBeforeBIC;
+			String srcBlobBIC = Utils.fetchBlob(repo, commitBBIC.getName(), pathBBIC);
+			String dstBlobBIC = Utils.fetchBlob(repo, commitBIC.getName(), pathBIC);
+			Run.initGenerators();
+			ITree srcBIC;
+			ITree dstBIC;
+			try {
+				srcBIC = new JdtTreeGenerator().generateFromString(srcBlobBIC).getRoot();
+				dstBIC = new JdtTreeGenerator().generateFromString(dstBlobBIC).getRoot();
+			} catch (Exception e) {
+				continue;
+			}
+			Matcher matchBIC = Matchers.getInstance().getMatcher(srcBIC, dstBIC);
+			matchBIC.match();
+
+			ActionGenerator gBIC = new ActionGenerator(srcBIC, dstBIC, matchBIC.getMappings());
+			gBIC.generate();
+
+			List<Action> actionsBIC = gBIC.getActions();
+			int max = 0;
+			for (Action action : actionsBIC) {
+				if (action.getNode().getType() == 40 || action.getNode().getType() == 26) {
+					continue;
+				}
+				if (action.getName().equals("INS")) {
+					max++;
+				} else if (action.getName().equals("DEL")) {
+					max++;
+				}
+			}
+
+			if (max > max_size) {
+				max_size = max;
+			}
+		}
+		walk.close();
+		return max_size;
+	}
+
 	public static void runD4j3(Input input)
 			throws IOException, RevisionSyntaxException, NoHeadException, GitAPIException {
 
@@ -264,216 +475,5 @@ public class Gumtree {
 		System.out.println("all_cnt: " + all_cnt);
 
 		System.out.println("writing gumvecs complete!");
-	}
-
-	public static void runGumtree(Input input, ArrayList<BeforeBIC> bbics)
-			throws MissingObjectException, IncorrectObjectTypeException, IOException {
-
-		Repository repo = input.repo;
-		RevWalk walk = new RevWalk(repo);
-		ArrayList<ArrayList<Integer>> gumtree_vectors = new ArrayList<ArrayList<Integer>>();
-		ArrayList<BeforeBIC> new_bbics = new ArrayList<BeforeBIC>();
-		File file_Y;
-		File file_GV;
-		if (Main.is_clean) {
-			file_Y = new File(input.outputDir + "Y_" + input.projectName + ".csv");
-			file_GV = new File(input.outputDir + "GVNC_" + input.projectName + ".csv");
-		} else {
-			file_Y = new File(input.outputDir + "Y_" + input.projectName + ".csv");
-			file_GV = new File(input.outputDir + "GVNC_" + input.projectName + ".csv");
-		}
-
-		BufferedWriter writer_Y = Files.newBufferedWriter(Paths.get(file_Y.getAbsolutePath()));
-		BufferedWriter writer_GV = Files.newBufferedWriter(Paths.get(file_GV.getAbsolutePath()));
-		CSVPrinter csvprinter_Y = new CSVPrinter(writer_Y, CSVFormat.DEFAULT);
-		CSVPrinter csvprinter_GV = new CSVPrinter(writer_GV, CSVFormat.DEFAULT);
-
-		int cnt = 0;
-		for (BeforeBIC bbic : bbics) {
-			RevCommit commitBIC = walk.parseCommit(repo.resolve(bbic.shaBIC));
-			RevCommit commitBBIC = walk.parseCommit(repo.resolve(bbic.shaBeforeBIC));
-			// RevCommit commitFIX = walk.parseCommit(repo.resolve(bbic.shaFix));
-			// RevCommit commitBFIX = walk.parseCommit(repo.resolve(bbic.shaFix + "^"));
-
-			String pathBIC = bbic.pathBIC;
-			String pathBBIC = bbic.pathBeforeBIC;
-			// String pathFIX = bbic.pathFix;
-			// String pathBFIX = bbic.pathBFix;
-
-			String srcBlobBIC = Utils.fetchBlob(repo, commitBBIC.getName(), pathBBIC);
-			String dstBlobBIC = Utils.fetchBlob(repo, commitBIC.getName(), pathBIC);
-			// String srcBlobFIX = Utils.fetchBlob(repo, commitBFIX.getName(), pathBFIX);
-			// String dstBlobFIX = Utils.fetchBlob(repo, commitFIX.getName(), pathFIX);
-
-			Run.initGenerators();
-
-			// for BIC
-			ITree srcBIC;
-			ITree dstBIC;
-			try {
-				srcBIC = new JdtTreeGenerator().generateFromString(srcBlobBIC).getRoot();
-				dstBIC = new JdtTreeGenerator().generateFromString(dstBlobBIC).getRoot();
-			} catch (Exception e) {
-				continue;
-			}
-			Matcher matchBIC = Matchers.getInstance().getMatcher(srcBIC, dstBIC);
-			matchBIC.match();
-
-			ActionGenerator gBIC = new ActionGenerator(srcBIC, dstBIC, matchBIC.getMappings());
-			gBIC.generate();
-
-			List<Action> actionsBIC = gBIC.getActions();
-
-			ArrayList<Integer> g_vec = new ArrayList<Integer>();
-			for (Action action : actionsBIC) {
-				// if regards import, discard
-				if (action.getNode().getType() == 40 || action.getNode().getType() == 26) {
-					continue;
-				}
-
-				if (action.getName().equals("INS")) {
-					g_vec.add(action.getNode().getType() + 1);
-				} else if (action.getName().equals("DEL")) {
-					g_vec.add(action.getNode().getType() + 85 + 1);
-				}
-			}
-
-			// using JDT parser to get the line number of AST nodes
-			@SuppressWarnings("deprecation")
-			ASTParser parser = ASTParser.newParser(AST.JLS9);
-			parser.setKind(ASTParser.K_COMPILATION_UNIT);
-			Hashtable<String, String> pOptions = JavaCore.getOptions();
-			pOptions.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_9);
-			pOptions.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM, JavaCore.VERSION_9);
-			pOptions.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_9);
-			pOptions.put(JavaCore.COMPILER_DOC_COMMENT_SUPPORT, JavaCore.ENABLED);
-			parser.setCompilerOptions(pOptions);
-			parser.setSource(dstBlobBIC.toCharArray());
-			CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-
-			// retrieving the context vector in AST
-			ArrayList<Integer> context_vec = new ArrayList<Integer>();
-			HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
-			for (Action action : actionsBIC) {
-				int lineNumOfBIC = cu.getLineNumber(action.getNode().getPos());
-				int parent_hash = action.getNode().getParent().getHash();
-				if (map.containsKey(parent_hash)) {
-					continue;
-				} else {
-					map.put(parent_hash, true);
-					List<ITree> descendants = action.getNode().getParent().getDescendants();
-					for (ITree descendant : descendants) {
-						if (map.containsKey(descendant.getHash())) {
-							continue;
-						} else {
-							int lineNumOfDescendant = cu.getLineNumber(descendant.getPos());
-							if (Math.abs(lineNumOfBIC - lineNumOfDescendant) < 3) {
-								map.put(descendant.getHash(), true);
-								context_vec.add(descendant.getType());
-							}
-						}
-					}
-				}
-			}
-
-			// adding the two lists
-			g_vec.addAll(context_vec);
-			System.out.println(cnt + ": " + g_vec.size());
-
-			gumtree_vectors.add(g_vec);
-			new_bbics.add(bbic);
-
-			csvprinter_Y.printRecord(input.projectName + cnt, bbic.pathBeforeBIC, bbic.pathBIC, bbic.shaBeforeBIC,
-					bbic.shaBIC, bbic.pathBeforeBFC, bbic.pathBFC, bbic.shaBeforeBFC, bbic.shaBFC, bbic.key,
-					input.projectName, bbic.label);
-
-			csvprinter_Y.flush();
-
-			for (Integer val : g_vec) {
-				csvprinter_GV.print(val);
-			}
-			csvprinter_GV.println();
-			csvprinter_GV.flush();
-
-			System.out.println(cnt + "/" + bbics.size());
-			cnt++;
-		}
-
-		System.out.println("writing gumvecs complete!");
-
-		csvprinter_Y.close();
-		csvprinter_GV.close();
-		walk.close();
-	}
-
-	public static void writeGumVecs(Input input, ArrayList<ArrayList<Integer>> gumtree_vectors) throws IOException {
-		File fileP = new File(input.outputDir + "GV_" + input.projectName + ".csv");
-		BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileP.getAbsolutePath()));
-		CSVPrinter csvprinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
-
-		int index = 0;
-		for (ArrayList<Integer> g_vec : gumtree_vectors) {
-			csvprinter.print(input.projectName + index);
-			for (Integer val : g_vec) {
-				csvprinter.print(val);
-			}
-			csvprinter.println();
-			csvprinter.flush();
-			index++;
-		}
-
-		csvprinter.close();
-		System.out.println("wrting gumvecs complete!");
-		return;
-	}
-
-	public static int getMaxASTLenth(ArrayList<BeforeBIC> bbics, Input input) throws RevisionSyntaxException,
-			MissingObjectException, IncorrectObjectTypeException, AmbiguousObjectException, IOException {
-		Repository repo = input.repo;
-		RevWalk walk = new RevWalk(repo);
-		int max_size = 0;
-		// get the max length of changes
-		for (BeforeBIC bbic : bbics) {
-			RevCommit commitBIC = walk.parseCommit(repo.resolve(bbic.shaBIC));
-			RevCommit commitBBIC = walk.parseCommit(repo.resolve(bbic.shaBeforeBIC));
-
-			String pathBIC = bbic.pathBIC;
-			String pathBBIC = bbic.pathBeforeBIC;
-			String srcBlobBIC = Utils.fetchBlob(repo, commitBBIC.getName(), pathBBIC);
-			String dstBlobBIC = Utils.fetchBlob(repo, commitBIC.getName(), pathBIC);
-			Run.initGenerators();
-			ITree srcBIC;
-			ITree dstBIC;
-			try {
-				srcBIC = new JdtTreeGenerator().generateFromString(srcBlobBIC).getRoot();
-				dstBIC = new JdtTreeGenerator().generateFromString(dstBlobBIC).getRoot();
-			} catch (Exception e) {
-				continue;
-			}
-			Matcher matchBIC = Matchers.getInstance().getMatcher(srcBIC, dstBIC);
-			matchBIC.match();
-
-			ActionGenerator gBIC = new ActionGenerator(srcBIC, dstBIC, matchBIC.getMappings());
-			gBIC.generate();
-
-			List<Action> actionsBIC = gBIC.getActions();
-			int max = 0;
-			for (Action action : actionsBIC) {
-				if (action.getNode().getType() == 40 || action.getNode().getType() == 26) {
-					continue;
-				}
-				if (action.getName().equals("INS")) {
-					max++;
-				} else if (action.getName().equals("DEL")) {
-					max++;
-				}
-			}
-
-			if (max > max_size) {
-				max_size = max;
-			}
-		}
-		walk.close();
-		return max_size;
 	}
 }
